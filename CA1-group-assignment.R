4cc73dfc1d7c944587ddd223cb2bd4af6ed75750
mayara-cct <2020292@student.cct.ie>
2023-12-04 17:56:00 +0000

    task F
    Applying dummy variable

--- CA1-group-assignment.R
+++ CA1-group-assignment.R
@@ -3,14 +3,12 @@ install.packages(c("tidyverse","dummy","caTools","conflicted"))
 install.packages("skimr")
 install.packages("caret")
 
-
 library(conflicted)
 library(tidyverse)
 library(dummy)
 library(caTools)
 library(skimr)
 library(caret)
-library(ggplot2)
 
 covid_df <- read_csv(file="covid_data_2023.csv")
 #Sourced from: https://teacherscollege.screenstepslive.com/a/1127011-display-your-data-in-r-studio
@@ -31,48 +29,21 @@ summary (covid_df)
 numeric_columns <- covid_df[,c(4,6,8,9)]
 
 #Impute NA
-#Source: https://www.tutorialspoint.com/how-to-remove-all-rows-having-na-in-r
+
 cleaned_data <- na.omit(numeric_columns)
 
-
-#question c
 preprocess_df <- preProcess(covid_df[,c(4,6,8,9)], method = c('center', 'scale'))
 data <- predict(preprocess_df, covid_df[,c(4,6,8,9)])
 summary (data)
 
-#question d
-
-
-# Scatter plot: based on sample tutorial
-ggplot(covid_df, aes(x=rate_14_day, y= country
-)) + geom_point(size=1, color="Blue")
-
-# Based on tutorial 5 - scatter plot
-covid_df %>%
-  ggplot() +
-  geom_point(
-    mapping = aes(x = rate_14_day, y = country),
-    size = 1,
-    color = "blue"
-  ) +
-  labs(title = "14 days rate", x = "rate_14_day", y = "country") +
-  theme_minimal() + 
-  theme(text = element_text(size = 14))
-
-#Line Plot
- 
-ggplot(covid_df, aes(x = rate_14_day, y = country)) +
-  geom_line(color = "red") +
-  labs(title = "14 days rate", x = "rate_14_day", y = "country")
-
-#heatmap
-ggplot(covid_df, aes(x = year_week, y = country, fill = rate_14_day)) +
-  geom_tile() +
-  labs(title = "Heatmap of 14-Day Rates by Country and Week",
-       x = "Year-Week",
-       y = "Country")
-
- covid_df.pca <- prcomp(cleaned_data, center = TRUE, scale. = TRUE)
+covid_df.pca <- prcomp(cleaned_data, center = TRUE, scale. = TRUE)
 summary (covid_df.pca)
+
+head(covid_df)
+
+covid_df$Indicator_Cases <- ifelse(covid_df$indicator == 'cases', 1, 0)
+covid_df$Indicator_Deaths <- ifelse(covid_df$indicator == 'deaths', 1, 0)
+
+view(covid_df)
 
 
