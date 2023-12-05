4bd56552f6cf4f3ac2ed9484d8cf7341dae0e925
Thayene-CCT <2020293@student.cct.ie>
2023-12-05 22:37:26 +0000

    Moving question G

--- CA1-group-assignment.R
+++ CA1-group-assignment.R
@@ -128,9 +128,6 @@ ggplot(covid_df, aes(x = year_week, y = country, fill = rate_14_day)) +
        y = "Country")
 
 
- covid_df.pca <- prcomp(cleaned_data, center = TRUE, scale. = TRUE)
-summary (covid_df.pca)
-
 #--------------------------Question F---------------------------------
 
 head(covid_df)
@@ -141,6 +138,11 @@ covid_df$Indicator_Deaths<- ifelse(covid_df$indicator == 'cases', 1, 0)
 covid_df$Indicator_Cases <- ifelse(covid_df$indicator == 'deaths', 1, 0)
 
 view(covid_df)
+
+#--------------------------Question G---------------------------------
+
+covid_df.pca <- prcomp(cleaned_data, center = TRUE, scale. = TRUE)
+summary (covid_df.pca)
 
 #--------------------------Question E---------------------------------
 
